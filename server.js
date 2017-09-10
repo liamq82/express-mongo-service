a4bb39087cb5d2bd4eb08933dacd4a464c2f1e3e
liamq82 <liamq82@gmail.com>
2017-09-10 18:29:07 +0100

    add routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var app = express();
 
@@ -9,15 +10,23 @@ var Customer = require('./models/customerModel');
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    Customer.find(function(err, customers){
-        if(err){
-            res.status(500).send(err);
-        }else {
-            res.json(customers);
-        }
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+router.route('/customers')
+    .get(function(req, res){
+        Customer.find(function(err, customers){
+            if(err){
+                res.status(500).send(err);
+            }else {
+                res.json(customers);
+            }
+        });
     });
-});
+app.use('/api', router);
 
 app.listen(port, function(){
     console.log('listening on port ' + port);
